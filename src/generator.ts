f08fc63e72413469ff4afca800e189e6cc012210
Adam Chawansky <adam.chawansky@gmail.com>
2024-02-07 11:23:16 -0800

    make permutations

--- src/generator.ts
+++ src/generator.ts
@@ -1,6 +1,7 @@
 // (0,0,0), (0,0,1), ... (0,0,5), (0,1,1), ... (5,5,5)
 
 import { NewBlock } from "./types";
+import { PlacedBlock } from "./types";
 
 export function makeNewBlocks(): NewBlock[] {
   const newBlocks: NewBlock[] = [];
@@ -12,4 +13,50 @@ export function makeNewBlocks(): NewBlock[] {
     }
   }
   return newBlocks;
+}
+
+// Given a NewBlock, return 3 permutations of As and Bs
+function permuteBlock( tile:NewBlock ): PlacedBlock[] {
+  return [{
+  // Make the 3 A types
+    topCenter: tile[0],
+    bottomLeft: tile[1],
+    bottomRight: tile[2],
+  }, {
+    topCenter: tile[1],
+    bottomLeft: tile[2],
+    bottomRight: tile[0],
+  }, {
+    topCenter: tile[2],
+    bottomLeft: tile[0],
+    bottomRight: tile[1],
+  }, {
+  // Make the 3 B types
+    bottomCenter: tile[0],
+    topLeft: tile[1],
+    topRight: tile[2],
+  }, {
+    bottomCenter: tile[1],
+    topLeft: tile[2],
+    topRight: tile[0],
+  }, {
+    bottomCenter: tile[2],
+    topLeft: tile[0],
+    topRight: tile[1],
+  }];
+}
+
+// Write a function that defines array of playspace
+// How to expand from the "center" of the board?
+// Or just allocate a fuckton of space and hope it's enough?
+// Or allocate a reasonable amount of space and recopy into an array with double dimensions if you ever hit the boundaries?
+
+
+// Write a function that given a new Block and placedBlock, do they align?
+function doBlocksAlign( testBlock: NewBlock, ): PlacedBlock|Boolean {
+  for( let i=0; i<)
+    if () {
+      return PlacedBlock;
+  }
+   return false;
 }
