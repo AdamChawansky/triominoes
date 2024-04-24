285c214c0dcf0b47bee802751a20442c718e92b6
Adam Chawansky <adam.chawansky@gmail.com>
2024-04-24 12:40:33 -0700

    New Game & Undo buttons

--- src/components/RootDisplay.tsx
+++ src/components/RootDisplay.tsx
@@ -7,9 +7,20 @@ import './Game.css';
 import { GameBoardView } from './GameBoardView.tsx';
 
 export function RootDisplay() {
-  const [gameHistory] = useState<GameHistory>(createGameHistory(1));
+  const [gameHistory, setGameHistory] = useState<GameHistory>(createGameHistory(1));
   const gameState = simulateHistory(gameHistory);
   const setGame = () => {};
+
+  function startNewGame() {
+    setGameHistory(createGameHistory(1));
+  }
+
+  function performUndo() {
+    setGameHistory({
+      startingDeck: gameHistory.startingDeck,
+      actions: gameHistory.actions.slice(0, -1),
+    });
+  }
 
   return (
     <main>
@@ -21,6 +32,16 @@ export function RootDisplay() {
         playerIndex={0}
         gameState={gameState} 
       />
+      <button
+        onClick={startNewGame}
+      >
+        NEW GAME
+      </button>
+      <button 
+        onClick={performUndo}
+      >
+        UNDO
+      </button>
       {/* todo game log, score, etc */}
     </main>
   );
