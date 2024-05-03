3188c18b6ff4dcdcfdb5150992efb17e5d271e48
Adam Chawansky <adam.chawansky@gmail.com>
2024-05-03 15:25:29 -0700

    ActionPusher

--- src/components/RootDisplay.tsx
+++ src/components/RootDisplay.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { initializeNewGameHistory, simulateCompleteGame, simulateOneAction, eraseGameHistory } from '../game/generator.ts';
 import { replayHistory } from '../game/history.ts';
-import { Action, ActionPusher, GameHistory } from '../game/types.ts';
+import { Action, GameHistory } from '../game/types.ts';
 import { DisplayHand } from './DisplayHand.tsx';
 import './Game.css';
 import { GameBoardView } from './GameBoardView.tsx';
