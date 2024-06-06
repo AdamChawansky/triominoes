00ddb4622e33f4fd83323af918dc5ab5eb90564e
Adam Chawansky <adam.chawansky@gmail.com>
2024-06-06 10:42:06 -0700

    Update How to Play with images

--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -1,23 +1,21 @@
 import './HowToPlay.css'
 
-interface HowToPlayButtonProps {
+export function HowToPlayButton(props: {
   onClick: () => void;
-}
-
-export function HowToPlayButton({ onClick }: HowToPlayButtonProps) {
+}) {
   return (
-    <button className="how-to-play-button" onClick={onClick}>
+    <button className="how-to-play-button" onClick={props.onClick}>
       HOW TO PLAY
     </button>
   );
 }
 
-interface HowToPlayPopupProps {
+export function HowToPlayPopup(props: {
   isOpen: boolean;
   onClose: () => void;
-}
+}) {
+  const { isOpen, onClose } = props;
 
-export function HowToPlayPopup({ isOpen, onClose }: HowToPlayPopupProps) {
   if (!isOpen) {
     return null;
   }
@@ -44,30 +42,135 @@ export function HowToPlayPopup({ isOpen, onClose }: HowToPlayPopupProps) {
                 <p>If no triple is held by any player, the player with the highest single tile value plays it and scores its value without the 10 point bonus. Tile values are
                 computed by summing the three numbers. For example, the 2-4-5 tile is worth 2+4+5=11 points. When two or more players hold tiles with the same
                 summed value, the next highest counts and so on until there is the highest value tile.</p>
+                
+                <div className="gallery">
+                  <div className="gallery-caption">Starting examples</div>
+                  <div className="gallery-items">
+                    <div className="gallery-item">
+                      <div className="gallery-item-thumb">
+                        <img
+                          src="//upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Triominoes_4-4-4.svg/112px-Triominoes_4-4-4.svg.png"
+                          alt="Placing a triple-4 tile to start gives the player a score of 22 points (4×3 + 10 point bonus)"
+                        />
+                      </div>
+                      <div className="gallery-item-text">
+                        Placing a triple-4 tile to start gives the player a score of 22 points (4×3 + 10 point bonus)
+                      </div>
+                    </div>
+                    <div className="gallery-item">
+                      <div className="gallery-item-thumb">
+                        <img
+                          src="//upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Triominoes_0-0-0.svg/112px-Triominoes_0-0-0.svg.png"
+                          alt="Placing a triple-0 tile to start gives the player a score of 40 points (30 points for triple-0 + 10 point bonus)"
+                        />
+                      </div>
+                      <div className="gallery-item-text">
+                        Placing a triple-0 tile to start gives the player a score of 40 points (30 points for triple-0 + 10 point bonus)
+                      </div>
+                    </div>
+                    <div className="gallery-item">
+                      <div className="gallery-item-thumb">
+                        <img
+                          src="//upload.wikimedia.org/wikipedia/commons/thumb/5/54/Triominoes_2-4-5.svg/112px-Triominoes_2-4-5.svg.png"
+                          alt="Placing a 2-4-5 tile to start gives the player a score of 11 points (2+4+5, no bonus)"
+                        />
+                      </div>
+                      <div className="gallery-item-text">
+                        Placing a 2-4-5 tile to start gives the player a score of 11 points (2+4+5, no bonus)
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                
+            <h2>Tile Placement</h2>
+              <div className="gallery">
+                <div className="gallery-caption">Legal placement examples</div>
+                <div className="gallery-items">
+                  <div className="gallery-item">
+                    <div className="gallery-item-thumb">
+                      <img
+                        src="//upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Triominoes_placement_example_01.svg/200px-Triominoes_placement_example_01.svg.png"
+                        alt="The legal placement of the 3-4-4 tile scores 3+4+4=11 points. Note how the corner values are matched to the adjacent 4-4-4 tile."
+                      />
+                    </div>
+                    <div className="gallery-item-text">
+                      The legal placement of the 3-4-4 tile scores 3+4+4=11 points. Note how the corner values are matched to the adjacent 4-4-4 tile.
+                    </div>
+                  </div>
+                  <div className="gallery-item">
+                    <div className="gallery-item-thumb">
+                      <img
+                        src="//upload.wikimedia.org/wikipedia/commons/thumb/3/38/Triominoes_placement_example_02.svg/200px-Triominoes_placement_example_02.svg.png"
+                        alt="The legal placement of the 2-5-5 tile scores 12 points."
+                      />
+                    </div>
+                    <div className="gallery-item-text">
+                      The legal placement of the 2-5-5 tile scores 12 points.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <p>Play proceeds clockwise; each player places a tile from their hand that lines up with the already placed tiles. A valid play of a triomino is similar to that of a
+              domino in that the sides contacting each other must have matching numbers; this means the two numbers at the corners must match with the adjacent
+              tile. A side with a 1 and a 2 will often not match the side the player is attempting to match. If a new tile is being placed such that it would touch two other
+              tiles, then all of the adjacent numbers must match. Where the points of tiles meet the numbers must always be the same; if they are not, someone has been
+              allowed to misplace a tile and the round must be restarted.</p>
+              <p>Points are scored based on the numbers on the tile played, with additional bonuses applied when special placements are made. After a tile has been placed
+              legally, each number on the newly placed tile is summed and added to that player's score. (E.g. 5 + 3 + 1 = 9).</p>
+            <h2>Drawing Penalty</h2>
+              <p>If a player cannot place a piece using the tiles in their hand, they must draw a new piece from the remaining pile. Each tile drawn penalizes the player 5
+              points. Players must continue to draw until a matching tile is found or until they have drawn three tiles. If one of the three can be placed the score is the
+              value of the tile placed less the penalty, which may be positive or negative, depending on the placed tile value and number of tiles drawn. If none of these
+              three tiles can be placed the penalty is 25 points.</p>
+              <p>If there are no remaining tiles left to be drawn and a player is unable to place a tile, they lose 10 points. For example, if there are two tiles left, a player
+              without a tile to be placed may draw them (receiving a penalty -10 in total for the two tiles drawn), and if that player still has no tile that can be played, the
+              additional end penalty is incurred (total penalty -10 + -10).</p>
                     
-            <h2>Legal placement examples</h2>
-                <p>Play proceeds clockwise; each player places a tile from their hand that lines up with the already placed tiles. A valid play of a triomino is similar to that of a
-                domino in that the sides contacting each other must have matching numbers; this means the two numbers at the corners must match with the adjacent
-                tile. A side with a 1 and a 2 will often not match the side the player is attempting to match. If a new tile is being placed such that it would touch two other
-                tiles, then all of the adjacent numbers must match. Where the points of tiles meet the numbers must always be the same; if they are not, someone has been
-                allowed to misplace a tile and the round must be restarted.</p>
-                <p>Points are scored based on the numbers on the tile played, with additional bonuses applied when special placements are made. After a tile has been placed
-                legally, each number on the newly placed tile is summed and added to that player's score. (E.g. 5 + 3 + 1 = 9).</p>
-                <p>If a player cannot place a piece using the tiles in their hand, they must draw a new piece from the remaining pile. Each tile drawn penalizes the player 5
-                points. Players must continue to draw until a matching tile is found or until they have drawn three tiles. If one of the three can be placed the score is the
-                value of the tile placed less the penalty, which may be positive or negative, depending on the placed tile value and number of tiles drawn. If none of these
-                three tiles can be placed the penalty is 25 points.</p>
-                <p>If there are no remaining tiles left to be drawn and a player is unable to place a tile, they lose 10 points. For example, if there are two tiles left, a player
-                without a tile to be placed may draw them (receiving a penalty -10 in total for the two tiles drawn), and if that player still has no tile that can be played, the
-                additional end penalty is incurred (total penalty -10 + -10).</p>
-                    
-            <h2>Bonus placement examples</h2>
-                <p>When a player can place a tile that completes a closed hexagonal shape (i.e. the 6th piece & all 3 numbers match), that player receives a bonus of 50 points
-                plus the regular score for a legally placed tile, less any penalty if pieces have been drawn. If a player completes two hexagons in one move the player
-                receives a 100 point bonus.</p>
-                <p>Bridges are made by matching one side of the tile and the point opposite. If a bridge is completed or a tile is added to a bridge, that player receives a 40-
-                point bonus in addition to the tile's points, less any penalty if pieces have been drawn.</p>
-                    
+            <h2>Bonus Points</h2>
+              <p>When a player can place a tile that completes a closed hexagonal shape (i.e. the 6th piece & all 3 numbers match), that player receives a bonus of 50 points
+              plus the regular score for a legally placed tile, less any penalty if pieces have been drawn. If a player completes two hexagons in one move the player
+              receives a 100 point bonus.</p>
+              <p>Bridges are made by matching one side of the tile and the point opposite. If a bridge is completed or a tile is added to a bridge, that player receives a 40-
+              point bonus in addition to the tile's points, less any penalty if pieces have been drawn.</p>
+              <div className="gallery">
+                <div className="gallery-caption">Bonus placement examples</div>
+                <div className="gallery-items">
+                  <div className="gallery-item">
+                    <div className="gallery-item-thumb">
+                      <img
+                        src="//upload.wikimedia.org/wikipedia/commons/thumb/7/79/Triominoes_placement_example_03.svg/165px-Triominoes_placement_example_03.svg.png"
+                        alt="The completion of the hexagon with the 0-5-5 tile scores 0+5+5 + 50 (bonus) = 60 points in total. Note how all three values on the placed 0-5-5 tile must match the two adjacent tiles."
+                      />
+                    </div>
+                    <div className="gallery-item-text">
+                      The completion of the hexagon with the 0-5-5 tile scores 0+5+5 + 50 (bonus) = 60 points in total. Note how all three values on the placed 0-5-5 tile must match the two adjacent tiles.
+                    </div>
+                  </div>
+                  <div className="gallery-item">
+                    <div className="gallery-item-thumb">
+                      <img
+                        src="//upload.wikimedia.org/wikipedia/commons/thumb/8/87/Triominoes_placement_example_04.svg/139px-Triominoes_placement_example_04.svg.png"
+                        alt="The completion of the bridge with the 1-4-4 tile scores 1+4+4 + 40 (bonus) = 49 points in total. Note how the corner value 1 must match the adjacent corners."
+                      />
+                    </div>
+                    <div className="gallery-item-text">
+                      The completion of the bridge with the 1-4-4 tile scores 1+4+4 + 40 (bonus) = 49 points in total. Note how the corner value 1 must match the adjacent corners.
+                    </div>
+                  </div>
+                  <div className="gallery-item">
+                    <div className="gallery-item-thumb">
+                      <img
+                        src="//upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Triominoes_placement_example_05.svg/200px-Triominoes_placement_example_05.svg.png"
+                        alt="The completion of the bridge with the 1-2-5 tile scored 1+2+5+40 = 48 points. The bridge was then augmented with the 1-1-5 tile, which scored 1+1+5+40 = 47 points, as that player was required to match two adjacent sides."
+                      />
+                    </div>
+                    <div className="gallery-item-text">
+                      The completion of the bridge with the 1-2-5 tile scored 1+2+5+40 = 48 points. The bridge was then augmented with the 1-1-5 tile, which scored 1+1+5+40 = 47 points, as that player was required to match two adjacent sides.
+                    </div>
+                  </div>
+                </div>
+              </div>
+
             <h2>Round ending and objective</h2>
                 <p>The round ends when no player can place a tile, whether or not all the face-down tiles have been drawn, or when one player runs out of tiles.</p>
                 <ol>
